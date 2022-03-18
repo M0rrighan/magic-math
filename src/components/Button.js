b9b36d6dde56567c8b2101d0fb9d9459fab41851
M0rrighan <81151384+M0rrighan@users.noreply.github.com>
2022-03-18 22:51:52 +0100

    Refactor Button.js - class component to functional component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,34 +1,25 @@
-/* eslint-disable no-useless-constructor */
-/* eslint-disable react/prefer-stateless-function */
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-export default class Button extends Component {
-  constructor(props) {
-    super(props);
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick() {
-    const { updateCalculator, buttonName } = this.props;
+const Button = ({ updateCalculator, buttonName }) => {
+  function handleClick() {
     updateCalculator(buttonName);
   }
 
-  render() {
-    const { buttonName } = this.props;
-    return (
-      <button
-        type="button"
-        className="btn"
-        onClick={this.handleClick}
-      >
-        {buttonName}
-      </button>
-    );
-  }
-}
+  return (
+    <button
+      type="button"
+      className="btn"
+      onClick={handleClick}
+    >
+      {buttonName}
+    </button>
+  );
+};
 
 Button.propTypes = {
   buttonName: PropTypes.string.isRequired,
   updateCalculator: PropTypes.func.isRequired,
 };
+
+export default Button;
